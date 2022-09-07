40e2d8acff51afd001bc211688c37e5f4a17af0e
sivan spiel <sivan.spiel@cooper.edu>
2022-09-07 18:52:03 -0400

    lab2

---
+++ cs102/lab-2/lab2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main( int argc, char **argv)
+
+{
+
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+fprintf( stdout, "hello, world :)\n");
+
+
+return 0;
+
+}
