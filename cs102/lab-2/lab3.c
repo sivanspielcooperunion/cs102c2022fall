97163e5e9277edecc11b08742f8ccc44e469a9e1
sivan spiel <sivan.spiel@cooper.edu>
2022-09-07 18:52:30 -0400

    lab3

---
+++ cs102/lab-2/lab3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main( int argc, char **argv)
+
+{
+int i = 0;
+ int sum = 0;
+ while( i < 10 )
+ {
+
+
+fprintf( stdout, "hello, world :)\n");
+i = i +1;
+
+}
+
+return 0;
+
+}
