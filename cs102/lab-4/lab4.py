a93373968f10b80131f6e008e09d5d7cd3b77d08
abadneagu <leslie.abadneagu@cooper.edu>
2022-09-21 16:45:27 -0400

    lab4

---
+++ cs102/lab-4/lab4.py
@@ -0,0 +1,20 @@
+import tkinter
+tk = tkinter.Tk()
+c = tkinter.Canvas( tk, width=400, height=600 )
+c.pack()
+
+
+def func (f):
+    return f*f+1
+
+
+x= -200
+while x<201 :
+    y= func (x)
+    
+    c.create_rectangle( x, y/66, x+1, (y+1)/66 )
+    x = x + 1
+    
+
+    
+tk.mainloop()
