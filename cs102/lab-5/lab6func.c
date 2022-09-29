03f9bfd9c7cfef96853e596c569fee756824151d
abadneagu <leslie.abadneagu@cooper.edu>
2022-09-28 21:26:39 -0400

    lab6func.c

---
+++ cs102/lab-5/lab6func.c
@@ -0,0 +1,47 @@
+
+
+void fill( int array[5][5]);
+    {
+        int r;
+     /*    r= mrand48();*/
+         int j=0;
+         int i=0;
+	 int g=0;
+     for (g=0; g<5; g=g+1)
+     {
+         for (i=0; i<5;i=i+1)
+        {
+         r=mrand48();
+          array[i][g]=(r%100);
+        }
+     }
+    }
+void print(int array[5][5]);
+{
+      int i=0;
+      int s=0;
+      int j=0;
+      for (j=0; j<5;j=j+1)
+      {
+        for (s=0; s<5; s=s+1)
+        {
+          fprintf(stdout, "%5d ",  array[j][s]);
+
+        }
+           fprintf( stdout, "\n");
+
+      }
+}
+
+void add(int array[5][5], int array2[5][5], int array3[5][5]);
+{
+	int a,b;
+for(a=0; a<5; a=a+1)
+	{
+ 	  for (b=0; b<5; b=b+1)
+		{
+		  matrix3[a][b] = matrix2[a][b] + matrix1[a][b];
+                }
+
+        }
+}
