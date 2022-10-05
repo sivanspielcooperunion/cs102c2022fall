05080fecd498c01b113a018db4d8d0de1edddb4b
jubaid hossain <jubaidhossain@github.com>
2022-10-05 17:41:20 -0400

    lab4

---
+++ cs102/lab-6/lab4.c
@@ -0,0 +1,34 @@
+/* Sivan Spiel
+        cs102c2022
+        10-5-22
+        prof cusack
+*/
+
+
+#include <stdio.h>
+int main( int argc, char** argv )
+
+{
+int i=1;
+
+if(argc>1)
+{
+  for( i=argc-1; i>0 ;i=i-1)
+  {
+        fprintf( stdout, "%s ", argv[i] );
+  }
+
+}
+else
+{
+        fprintf( stdout, "not args found ");
+}
+
+
+
+ fprintf( stdout, "\n" );
+  return 0;
+}
+
+
+
