17bc27b1f041280caf50df9f640a76caa633d0ce
jubaid hossain <jubaidhossain@github.com>
2022-10-05 17:52:06 -0400

    lab5a

---
+++ cs102/lab-6/lab5.c
@@ -0,0 +1,40 @@
+/* Sivan Spiel
+        cs102c2022
+        10-5-22
+        prof cusack
+*/
+
+
+#include <stdio.h>
+int string_length(char* string);
+
+int main( int argc, char** argv )
+
+{
+int i=1;
+
+if(argc>1)
+{
+  for( i=argc-1; i>0 ;i=i-1)
+  {
+        fprintf( stdout, "%s %d \n ", argv[i], string_length(argv[i]) );
+  }
+
+}
+else
+{
+        fprintf( stdout, "not args found ");
+}
+
+
+
+ fprintf( stdout, "\n" );
+  return 0;
+}
+
+
+
+int string_length(char* string)
+{
+return 42;
+}
