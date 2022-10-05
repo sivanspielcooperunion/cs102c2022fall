16ecff3195dc872f1056d96fab9708446e677b9c
jubaid hossain <jubaidhossain@github.com>
2022-10-05 17:15:06 -0400

    lab3

---
+++ cs102/lab-6/lab2c.c
@@ -0,0 +1,26 @@
+
+/* Sivan Spiel
+        cs102c2022
+        10-5-22
+        prof cusack
+*/
+
+
+#include <stdio.h>
+int main( int argc, char** argv )
+
+{
+int i=1;
+
+
+  for( i=1; i<argc;i=i+1)
+  {
+  	fprintf( stdout, "%s ", argv[i] );
+  /*	fprintf( stdout, "\n" ); */
+
+  }
+  return 0;
+}
+
+
+
