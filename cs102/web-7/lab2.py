dfa63e80b8c820c2f68eb631c7ea4a83553fefac
sivanspielcooperunion <sivanspielcooperunion@github.com>
2022-10-12 23:52:35 +0000

    lab2

---
+++ cs102/web-7/lab2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+print( "Content-type: text/html\n\n" )
+print( """
+<html>
+<head>
+<title>LAB7<title>
+</head>
+<body>
+<h1>my header</h1>
+""" )
+
+
+a= 0 
+while(a<100):
+	print("<p>%s</p>" % (a) )
+	a=a+1
+
+print( """
+</body>
+</html>
+""" )
